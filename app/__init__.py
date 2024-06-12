8282621be852d144b63f48412fa12df40be0c87a
Dámaris <a_dmontoya@colegiosanlorenzo.org>
2024-06-12 14:06:55 -0400

    mi primer commit

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+"""Configuración de la aplicacioón"""
+
+#Flask
+from flas import Flask
+
+app = Flask(__name__)
---
+++ server.py
@@ -0,0 +1,8 @@
+"""Server app."""
+
+from app import app
+
+# Ejecutar la aplicación 
+if __name__="__main__":
+    app.run(debug=True)
+
